6ea25c4c786175afd889c326c6ea8ebfc8540b52
Azaya89 <akoritaisaiah@gmail.com>
2022-04-20 16:58:51 +0100

    made output files more readable.

--- myscript.py
+++ myscript.py
@@ -58,7 +58,7 @@ def print_article(wd_item, repo=repo):
 
     #Print title and Q identifier of article.
     try:
-        print('\nTitle of article: \n' + page_info['labels']['en'])
+        print('\nTitle of article: \n\t' + page_info['labels']['en'])
         print('\nQ identifier of article:\n \t' + item_code + '\n')
     except:
         print('Item Name: This item has no English label!' + '\n')
@@ -115,7 +115,7 @@ article = get_page('Q51538190')
 my_page = 'User:Azaya89/Outreachy 1'
 if __name__ == "__main__":
     #This prevents these functions from being executed if the module is imported into another file. It runs only when this module is called directly.
-    print_my_page(my_page)
-    edit_article(my_page)
+    # print_my_page(my_page)
+    # edit_article(my_page)
     print_article(article)
-    print_all_articles(articles)
+    # print_all_articles(articles)
